8999fac34b0570f831cfc55ce00d514b4cbeffed
imjamesruiz <jamesruiz610@gmail.com>
2025-04-26 23:42:17 -0700

    search username function

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -138,3 +138,17 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     db.delete(db_user)
     db.commit()
     return {"message":"Deleted User"}
+
+
+# search for user (username)
+@router.get("/search/username/{username}", response_model=List[schemas.UserResponse])
+def search_user(username: str, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username.ilike(f"%{username}%")).all()
+    if db_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail = "User not found"
+            )
+    return db_user
+
+
